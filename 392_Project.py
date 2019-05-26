9071b96a8e6718d97378a472bdda502440a31d7e
Michael <michaelj1815@gmail.com>
2019-05-25 19:39:00 -0700

    392_Project_2

--- 392_Project.py
+++ 392_Project.py
@@ -6,16 +6,24 @@ print(df.head(10))
 
 #Saves the max value in column total profit to variable max_profit_amount
 max_profit_amount = (df['Total Profit'].max())
-print(max_profit_amount)
 
 #Searches for the row im which Total Profit is equal to max_profit_amount
-max_profit_id = df.loc[df['Total Profit'] == max_profit_amount]
-print(max_profit_id)
+#Saves the index of the row df_make_profit_id as a value
+df_max_profit_id = df.loc[df['Total Profit'] == max_profit_amount]
+index_max_profit = (df_max_profit_id.index.values)
 
-#Converts panda's datafram row to dict where keys are column names, values are lists of column data
-dict_max_profit_id = max_profit_id.to_dict('list')
-print(dict_max_profit_id)
-print(dict_max_profit_id['Order ID']
+#Converts panda's datafram row to dict where keys are column names and values are lists of column data
+#Assigns max_profit_id the value of the key Order ID
+dict_max_profit_id = df_max_profit_id.to_dict('list')
+max_profit_id = (dict_max_profit_id['Order ID'])
+
+id_max_profit = df.loc[df['Total Profit'] == max_profit_amount, 'Order ID'].item()
+
+print(f"The max profit is: ${max_profit_amount}")
+print(f"Found in order #: {id_max_profit}")
+print(f"At index value: {index_max_profit}")
+
+
 
 #print(df.loc[1][1])
 #print(df.loc[1]['Country'])
